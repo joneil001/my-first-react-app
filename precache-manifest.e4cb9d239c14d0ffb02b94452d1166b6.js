01e747a14dfddd9fad322c2f5d96325018c9d4c8
James O'Neill <james_oneil@cable.comcast.com>
2020-07-02 11:33:24 -0600

    Updates

---
+++ precache-manifest.e4cb9d239c14d0ffb02b94452d1166b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84633c0928d043f574061e46bffb789",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "99e418ab963a51953efe",
+    "url": "/my-first-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a469888bf3c34e6f20ff",
+    "url": "/my-first-react-app/static/js/2.12b8cc34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react-app/static/js/2.12b8cc34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e418ab963a51953efe",
+    "url": "/my-first-react-app/static/js/main.e29770a7.chunk.js"
+  },
+  {
+    "revision": "9fa5d64e5b1bb9a77b80",
+    "url": "/my-first-react-app/static/js/runtime-main.c3e2ea89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
